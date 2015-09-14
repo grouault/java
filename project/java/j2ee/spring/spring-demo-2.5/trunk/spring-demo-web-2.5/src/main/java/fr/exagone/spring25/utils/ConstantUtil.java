e9069e5b9a76cf27106b0f52774ffba56eda240c
rouaul_g <rouaul_g@451ca551-2b01-48a8-bb59-7062f3cb1b21>
2015-09-14 21:01:27 +0000

    Initial import.

---
+++ project/java/j2ee/spring/spring-demo-2.5/trunk/spring-demo-web-2.5/src/main/java/fr/exagone/spring25/utils/ConstantUtil.java
@@ -0,0 +1,12 @@
+package fr.exagone.spring25.utils;
+
+public class ConstantUtil {
+
+	
+	/** Format de la date utilisé dans les formulaires. */
+	public static final String DATE_FORMAT = "dd/MM/yyyy";
+	
+	/** Longueur max des champs date (pour les CustomDateEditors). */
+	public static final int DATE_LENGTH = 10;	
+	
+}
