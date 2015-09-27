1a3fe37a33e78d772f6cdc14c9d9a7a6bb0dc235
rouaul_g <rouaul_g@451ca551-2b01-48a8-bb59-7062f3cb1b21>
2015-09-27 15:24:50 +0000

    delete

--- project/java/j2ee/hibernate/team-manager/trunk/src/main/resources/sql/create-shema.sql
+++
@@ -1,5 +0,0 @@
-create user 'teammanager'@'localhost' identified by 'teammanager';
-create database teammanager;
-grant all on teammanager.* to 'teammanager'@'localhost';
-use teammanager;
-show tables;
