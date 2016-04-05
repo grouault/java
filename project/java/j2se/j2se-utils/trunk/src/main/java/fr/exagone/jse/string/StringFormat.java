1f07f8bb80c678c83a82d3235320d066c63693e6
rouaul_g <rouaul_g@451ca551-2b01-48a8-bb59-7062f3cb1b21>
2016-04-05 16:14:09 +0000

    [Divers]

---
+++ project/java/j2se/j2se-utils/trunk/src/main/java/fr/exagone/jse/string/StringFormat.java
@@ -0,0 +1,28 @@
+package fr.exagone.jse.string;
+
+
+public class StringFormat {
+
+	
+	private static final String TEST_CHAINE = "'http://localhost:8080/widget/web/estim/carrousel-next-evenement/-/widgetcarrouselnextevent_WAR_estimportailportlet?_widgetcarrouselnextevent_WAR_estimportailportlet_idsd=34701'";
+	
+	public static void main(String[] args) {
+
+		String html = "";
+        int strSize = TEST_CHAINE.length();
+        System.out.println("strSize = " + strSize);
+        int lineNbCar = 65;
+        int nbIter = (int)Math.floor(strSize / lineNbCar) > 0 ? (int)Math.floor(strSize / lineNbCar) : 1;
+        System.out.println("nbIter = " + nbIter);
+        for (int i = 0; i <=  nbIter; i++) {
+        	if (i < nbIter) {
+        		html += TEST_CHAINE.substring(i*lineNbCar, i*lineNbCar + lineNbCar) + "\n" + "\t" + "\t" + "\t";
+        	} else {
+        		html += TEST_CHAINE.substring(i*lineNbCar, TEST_CHAINE.length()) + "\n" + "\t" + "\t" + "\t";	
+        	}
+        }
+        System.out.println("html = " + html);	
+	}
+	
+	
+}
