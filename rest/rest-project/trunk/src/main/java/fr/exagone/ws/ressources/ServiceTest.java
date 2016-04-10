776d5a4ef4e492297c0eb6be2ea9825222767588
rouaul_g <rouaul_g@451ca551-2b01-48a8-bb59-7062f3cb1b21>
2016-04-10 21:22:18 +0000

    create

--- rest/rest-project/trunk/src/main/java/fr/exagone/ws/ressources/ServiceTest.java
+++ rest/rest-project/trunk/src/main/java/fr/exagone/ws/ressources/ServiceTest.java
@@ -13,8 +13,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import fr.exagone.entities.TitreMusique;
 /*
