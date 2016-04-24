6d8be6f900de01cb1a1438dba3c007addf3298c4
rouaul_g <rouaul_g@451ca551-2b01-48a8-bb59-7062f3cb1b21>
2016-04-24 15:44:06 +0000

    Test j2se

--- project/java/j2se/j2se-utils/trunk/src/main/java/fr/exagone/arrays/Tableau.java
+++ project/java/j2se/j2se-utils/trunk/src/main/java/fr/exagone/arrays/Tableau.java
@@ -7,6 +7,7 @@ public class Tableau {
 	public static void main(String[] args) {
 		
 		String[] strings = { "ABC", "DEF" };
+		System.out.println("Tab = " + strings.toString());
 		String s = Arrays.toString(strings); 
 		System.out.println("s = " + s);
 		
