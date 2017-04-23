65349e7834ca1fedc1a6b8159b5c4eb7ff2364d6
grouault <gildasrouault@gmail.com>
2017-04-23 17:15:22 +0200

    exagone-common

---
+++ j2se/exagone.common/src/main/java/fr/exagone/metier/Adresse.java
@@ -0,0 +1,37 @@
+package fr.exagone.metier;
+
+public class Adresse {
+
+	private String ville;
+	private Integer numeroRue;
+	private String rue;
+	private String codePostal;
+	
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public Integer getNumeroRue() {
+		return numeroRue;
+	}
+	public void setNumeroRue(Integer numeroRue) {
+		this.numeroRue = numeroRue;
+	}
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public String getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	
+	
+	
+}
